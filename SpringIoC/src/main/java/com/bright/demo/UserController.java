964753cb3599d0b7a07d3c8aaa0e561aa5b84533
徐亮亮 <1626769688@qq.com>
2020-11-14 12:31:32 +0800

    4. Spring相关API

--- SpringIoC/src/main/java/com/bright/demo/UserController.java
+++ SpringIoC/src/main/java/com/bright/demo/UserController.java
@@ -4,6 +4,7 @@ import com.bright.service.UserService;
 import com.bright.service.impl.UserServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * @author 徐亮亮
@@ -13,7 +14,9 @@ public class UserController {
 
     public static void main(String[] args) {
         ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserService userService = (UserService) app.getBean("userService");
+        // ApplicationContext app = new FileSystemXmlApplicationContext("D:\\IdeaProjects\\SpringStudyFromITHeiMA\\SpringIoC\\src\\main\\resources\\applicationContext.xml");
+        // UserService userService = (UserService) app.getBean("userService");
+        UserService userService = app.getBean(UserService.class);
         userService.save();
     }
 }
